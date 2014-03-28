6efe0803126fbf256d593ac9d03cc1bdad30c647
Harish Yeluri <hyeluri@gmail.com>
2014-03-27 21:52:48 -0700

    virutal server config

--- app/mongDb.js
+++ app/mongDb.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
  
-var path = process.env.MONGODB ||'mongodb://localhost/shorty';
+var path = process.env.MONGODB ||'mongodb://127.0.0.1:27017/shorty';
 
 mongoose.connect(path, function(err){
 
